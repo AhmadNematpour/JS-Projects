5d8fd853479b50d5bf40b0751c5981c6d77540af
Ahmad Nematpour <93655950+AhmadNematpour@users.noreply.github.com>
2023-09-12 22:40:54 +0330

    Delete (7) Sidebar directory

--- (7) Sidebar/(3) Error 404 Not Found/js/script.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector('#container');
-window.onmousemove = function(e) {
-    let x = e.clientX/5;
-    let y = e.clientY/5;
-
-    container.style.backgroundPositionX = x + "px";
-    container.style.backgroundPositionY = y + "px";
-}
