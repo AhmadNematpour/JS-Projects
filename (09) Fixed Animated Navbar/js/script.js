82b3100982e9822747866cab61226297ca064a5e
Ahmad Nematpour <93655950+AhmadNematpour@users.noreply.github.com>
2023-09-12 22:46:23 +0330

    Add files via upload

---
+++ (09) Fixed Animated Navbar/js/script.js
@@ -0,0 +1,21 @@
+
+window.addEventListener('scroll',() => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
