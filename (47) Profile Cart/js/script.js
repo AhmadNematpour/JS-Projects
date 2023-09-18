a9260248079812a9e5067da5a7ca21542a8716fd
Ahmad Nematpour <93655950+AhmadNematpour@users.noreply.github.com>
2023-09-18 23:42:38 +0330

    Add files via upload

---
+++ (47) Profile Cart/js/script.js
@@ -0,0 +1,38 @@
+
+const img = document.querySelector('img');
+const icon = document.querySelector('.icon');
+
+
+img.onclick = () => {
+    img.classList.toggle('active');
+    icon.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
