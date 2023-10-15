deb0f0b1b584aa2ade561f282ef89e36961ef328
Ahmad Nematpour <93655950+AhmadNematpour@users.noreply.github.com>
2023-10-15 22:47:13 +0330

    Add files via upload

---
+++ (65) Code Editor/js/script.js
@@ -0,0 +1,51 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const bar = document.querySelector(".bar");
+const editor = document.querySelector(".editor");
+const run = document.querySelector(".btn-run");
+const iframe = document.querySelector(".iframe");
+const dark = document.querySelector(".btn-dark");
+const light = document.querySelector(".btn-light");
+
+const drag = (e) => {
+  e.preventDefault();
+  if (document.selection) {
+    document.selection.empty();
+  } else {
+    window.getSelection().removeAllRanges();
+  }
+  // document.selection ? document.selection.empty() : window.getSelection().removeAllRanges();
+  left.style.width = e.pageX - bar.offsetWidth / 3 + "px";
+  editor.resize();
+};
+
+bar.addEventListener("mousedown", function () {
+  document.addEventListener("mousemove", drag);
+});
+
+bar.addEventListener("mouseup", function () {
+  document.removeEventListener("mousemove", drag);
+});
+
+run.addEventListener("click", () => {
+  const html = editor.textContent;
+  iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+});
+
+dark.addEventListener("click", function () {
+  editor.style.backgroundColor = "#333";
+  editor.style.color = "#fff";
+});
+light.addEventListener("click", function () {
+  editor.style.backgroundColor = "#fff";
+  editor.style.color = "#333";
+});
+
+document.getElementById("live").onclick = function() {
+  if (this.checked) {
+    editor.addEventListener("keyup", () => {
+      const html = editor.textContent;
+      iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+    });
+  }
+};
